ba6267f717e817ab145507380d0643886a2010cd
Alan Devine <alanrdevine@gmail.com>
2020-12-02 11:19:17 +0000

    Implemented extended gcd & multiplicitive inverse methods

--- src/Assignment2Utils.java
+++ src/Assignment2Utils.java
@@ -9,38 +9,50 @@ public class Assignment2Utils {
         return a.toByteArray();
     }
 
-    public static BigInteger gcd(BigInteger a, BigInteger b) {
-        return (b.intValue() == 0) ? a : gcd(b, a.mod(b));
-    }
+    public static BigInteger[] eGCD(BigInteger a, BigInteger b) {
+        // gcd(a, b) = ax + by
 
-    public static BigInteger gcdOfN(BigInteger[] n) {
-        // GCD implemented for n number of BigIntegers
-        BigInteger sol = n[0];
+        BigInteger[] sol = new BigInteger[3];
 
-        for (int i = 0; i < n.length; i++)
-            sol = gcd(sol, n[i]);
+        if (b.intValue() == 0) {
+            sol[0] = a;
+            sol[1] = BigInteger.ONE;
+            sol[2] = BigInteger.ZERO;
+            return sol;
+        }
+
+        BigInteger[] tmp = eGCD(b, a.mod(b));
+
+        // gcd
+        sol[0] = tmp[0];
+        // x coefficient
+        sol[1] = tmp[2];
+        // y coefficient
+        sol[2] = tmp[1].subtract(a.divide(b).multiply(tmp[2]));
 
         return sol;
     }
 
-    public static BigInteger multiplicativeInverse(BigInteger a, BigInteger b) {
-        return a;
+    public static BigInteger gcd(BigInteger a, BigInteger b) {
+        return (b.intValue() == 0) ? a : gcd(b, a.mod(b));
     }
 
-    public static BigInteger chineseRemainderTheorem(BigInteger a, BigInteger b) {
-        return a;
+    public static BigInteger modInverse(BigInteger a, BigInteger b) {
+        assert(gcd(a, b) == BigInteger.ONE);
+        BigInteger[] ans = eGCD(a, b);
+
+        if (ans[1].compareTo(BigInteger.ONE) != 0) return ans[1];
+        return ans[1].add(b);
     }
 
     public static void main(String[] args) {
-        BigInteger a = new BigInteger("4");
-        BigInteger b = new BigInteger("12");
-        BigInteger c = new BigInteger("6");
-        BigInteger d = new BigInteger("18");
+        BigInteger a = new BigInteger("3");
+        BigInteger b = new BigInteger("17");
+        BigInteger c = new BigInteger("1");
+        BigInteger d = new BigInteger("1");
 
-        BigInteger[] n = {a, b, c, d};
+        // BigInteger[] n = {a, b, c, d};
 
-        System.out.println(gcdOfN(n));
-        System.out.println(gcd(a, b));
-
+        System.out.println(modInverse(a, b));
     }
 }
