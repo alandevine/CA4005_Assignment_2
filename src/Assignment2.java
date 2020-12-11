d4ac5f2341eacf39dbef1403095ae4d8a147625e
Alan Devine <alanrdevine@gmail.com>
2020-12-11 13:12:55 +0000

    Finished Assignment2

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -1,9 +1,8 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigInteger;
-
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
@@ -13,87 +12,22 @@ import java.util.Random;
 
 public class Assignment2 {
 
+    /**
+     * Simple Method for writing a string to a file.
+     *
+     * @param fileName some filename
+     * @param value some string value
+     */
     public static void writeToFile(String fileName, String value) throws IOException {
         Files.write(Paths.get(fileName), Collections.singleton(value));
     }
 
-    public static String bytesToHex(byte[] bytes) {
-        StringBuilder buff = new StringBuilder();
-        for (byte b : bytes) {
-            buff.append(String.format("%02X", b));
-        }
-        return buff.toString();
-    }
-
-    public static byte[] BItoByte(BigInteger n) {
-        return n.toByteArray();
-    }
-
-    public static BigInteger decrypt(BigInteger c, BigInteger d, BigInteger p, BigInteger q) {
-
-        // m = c^d (mod n)
-
-        // My implementation of CRT takes arrays of big integers as input
-        BigInteger[] crypt_p = new BigInteger[] {CryptoMath.rightToLeftModExponentiation(c, d.mod(p.subtract(BigInteger.ONE)), p)};
-        BigInteger[] crypt_q = new BigInteger[] {CryptoMath.rightToLeftModExponentiation(c, d.mod(q.subtract(BigInteger.ONE)), q)};
-
-        return CryptoMath.chineseRemainderTheorem(crypt_p, crypt_q);
-
-    }
-
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException{
-
-        String inputFile = (args.length > 0) ? args[0] : null;
-
-        if (inputFile == null) {
-            System.out.println("Please provide a file to be encrypted.");
-            System.exit(0);
-        }
-
-        Random rand = new Random();
-
-        final BigInteger e = new BigInteger("65537");
-
-        BigInteger p, q, n, phi;
-
-        do {
-            p = BigInteger.probablePrime(512, rand);
-            q = BigInteger.probablePrime(512, rand);
-
-            n = p.multiply(q);
-
-            writeToFile("Modulus.txt", n.toString(16));
-
-            phi = CryptoMath.eulerTotient(p, q);
-        } while (CryptoMath.gcd(e, phi).intValue() != 1);
-
-        BigInteger d = CryptoMath.multInverse(e, phi);
-
-
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-
-        File ip = new File(inputFile);
-        InputStream is = new FileInputStream(ip);
-        byte[] inputBytes = is.readAllBytes();
-
-        BigInteger c = new BigInteger(1, inputBytes);
-
-        BigInteger signedDigest = decrypt(c, d, p, q);
-
-        System.out.println(signedDigest.toString(16));
-    }
-}
-
-class CryptoMath {
-
     /**
-     * Generate the Euler Totient of two BigIntegers p, and q. This method assumes that
-     * both p and q are probable primes and calculates phi based on the equation
-     * phi(p*q) = phi(p)*phi(q)
+     * Method for finding the euler totient of two probable primes.
      *
-     * @param p Probable Prime of Type BigInteger
-     * @param q Probable Prime of Type BigInteger
-     * @return returns phi(p*q) of type BigInteger
+     * @param p a Probable Prime with the type, BigInteger.
+     * @param q a Probable Prime with the type, BigInteger.
+     * @return returns the euler totient of p and q.
      */
     public static BigInteger eulerTotient(BigInteger p, BigInteger q) {
         BigInteger phiOfP, phiOfQ;
@@ -102,15 +36,36 @@ class CryptoMath {
         phiOfQ = q.subtract(BigInteger.ONE);
 
         return phiOfP.multiply(phiOfQ);
-
     }
 
     /**
-     * Implementation of the Extended Euclidean Algorithm
+     * Method for performing right-to=left modular exponentiation
      *
-     * @param a Some BigInteger a
-     * @param b Some BigInteger b
-     * @return BigInteger Array containing gcd, x coefficient and y coefficient
+     * @param a some BigInteger
+     * @param exp some exponent of the type BigInteger
+     * @param mod some modulus of the type BigInteger
+     * @return returns the modular exponentiation of the input values.
+     */
+    public static BigInteger modExponent(BigInteger a, BigInteger exp, BigInteger mod) {
+
+        int k = exp.bitLength();
+
+        BigInteger y = new BigInteger("1");
+
+        for (int i = k - 1; i >= 0; i--){
+            y = y.multiply(y).mod(mod);
+            y = (exp.testBit(i)) ? y.multiply(a).mod(mod) : y;
+        }
+
+        return y;
+    }
+
+    /**
+     * Method for performing the Extended Euclidean Algorithm.
+     *
+     * @param a some BigInteger a
+     * @param b some BigInteger b
+     * @return returns an array of BigInteger's containing {gcd, x coefficient, y coefficient}
      */
     public static BigInteger[] eGCD(BigInteger a, BigInteger b) {
         // gcd(a, b) = ax + by
@@ -136,55 +91,85 @@ class CryptoMath {
         return sol;
     }
 
+    /**
+     * Method for preforming standard gcd Algorithm
+     *
+     * @param a some BigInteger a
+     * @param b some BigInteger b
+     * @return returns the greatest common divisor of the BigIntegers `a` and `b`
+     */
     public static BigInteger gcd(BigInteger a, BigInteger b) {
         return (b.intValue() == 0) ? a : gcd(b, a.mod(b));
     }
 
+    /**
+     * Method for finding the multiplicative inverse
+     *
+     * @param a some number a
+     * @param b some modulus b
+     * @return returns the multiplicative inverse of a number `a` and a modulus `b`
+     */
     public static BigInteger multInverse(BigInteger a, BigInteger b) {
         BigInteger[] ans = eGCD(a, b);
-        return (ans[1].compareTo(BigInteger.ONE) < 0) ? ans[1].add(b) : ans[1];
+        return (ans[1].intValue() == 1) ? ans[1].add(b) : ans[1];
     }
 
-    public static BigInteger chineseRemainderTheorem(BigInteger[] b, BigInteger[] n) {
+    /**
+     * Method for finding a message signature using Chinese Remainder Theorem
+     * 
+     * @param c digest of some input with the type BigInteger
+     * @param d a decryption component
+     * @param p a public key with the type BigInteger
+     * @param q a private key with the type BigInteger
+     * @return returns a signed digest
+     */
+    public static BigInteger decrypt(BigInteger c, BigInteger d, BigInteger p, BigInteger q) {
+        BigInteger crypt_p = modExponent(c, d.mod(p.subtract(BigInteger.ONE)), p);
+        BigInteger crypt_q = modExponent(c, d.mod(q.subtract(BigInteger.ONE)), q);
 
-        assert(b.length == n.length);
+        BigInteger qInv = multInverse(q, p);
 
-        BigInteger[] N = new BigInteger[n.length];
-        BigInteger[] Y = new BigInteger[b.length];
-
-        BigInteger nProd = BigInteger.ONE;
-
-        for (BigInteger i : n)
-            nProd = nProd.multiply(i);
-
-        for (int i = 0; i < n.length; i++) {
-            N[i] = nProd.divide(n[i]);
-            Y[i] = multInverse(N[i], (n[i]));
-        }
-
-        BigInteger sum = BigInteger.ZERO;
-
-        for (int i = 0; i < n.length; i++)
-            sum = sum.add(b[i].multiply(N[i].multiply(Y[i])));
-
-        return sum.mod(nProd);
+        return crypt_p.add(q.multiply(qInv.multiply(crypt_p.subtract(crypt_q)).mod(p)));
     }
 
-    public static BigInteger rightToLeftModExponentiation(BigInteger a, BigInteger exp, BigInteger mod) {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
 
-        /*
-         * This method implements right-to-left modular exponentiation
-         * */
+        String inputFile = (args.length > 0) ? args[0] : null;
 
-        int k = exp.bitLength();
-
-        BigInteger y = new BigInteger("1");
-
-        for (int i = k - 1; i >= 0; i--){
-            y = y.multiply(y).mod(mod);
-            y = (exp.testBit(i)) ? y.multiply(a).mod(mod) : y;
+        if (inputFile == null) {
+            System.out.println("Please provide a file to be encrypted.");
+            System.exit(0);
         }
 
-        return y;
+        Random rand = new Random();
+
+        final BigInteger e = new BigInteger("65537");
+
+        BigInteger p, q, n, phi;
+
+        do {
+            p = BigInteger.probablePrime(512, rand);
+            q = BigInteger.probablePrime(512, rand);
+            n = p.multiply(q);
+
+            phi = eulerTotient(p, q);
+        } while (gcd(e, phi).intValue() != 1);
+
+        writeToFile("Modulus.txt", n.toString(16));
+
+        BigInteger d = multInverse(e, phi);
+
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        File ip = new File(inputFile);
+        InputStream is = new FileInputStream(ip);
+        byte[] inputBytes = is.readAllBytes();
+        is.close();
+
+        BigInteger c = new BigInteger(1, md.digest(inputBytes));
+
+        BigInteger signedDigest = decrypt(c, d, p, q);
+
+        System.out.println(signedDigest.toString(16));
     }
 }
